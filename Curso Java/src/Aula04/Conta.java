f76d1d9cb1e5d91feab11515b725709d223f9fb8
Pedro Henrique <pedrohenriquearaujo@github.com>
2018-01-18 16:23:44 -0300

    Utilizando OO, Classe Conta

--- Curso Java/src/Aula04/Conta.java
+++ Curso Java/src/Aula04/Conta.java
@@ -1,5 +1,28 @@
 package Aula04;
 
 public class Conta {
-
+	
+	private String numero;
+	private double saldo;
+	
+	
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}	
+	public void debitar (double valor) {		
+		this.saldo -= valor;
+	}	
+	public void cretidar (double valor) {
+		this.saldo += valor;
+	}	
+	
 }
