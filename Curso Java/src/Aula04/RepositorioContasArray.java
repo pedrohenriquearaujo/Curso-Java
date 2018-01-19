32f1f9879badfe382aa72b5d24a81cceda9b58db
Aluno <Aluno@Lab>
2018-01-18 21:36:10 -0300

    Atualização do Classe Repositorio

--- Curso Java/src/Aula04/RepositorioContasArray.java
+++ Curso Java/src/Aula04/RepositorioContasArray.java
@@ -2,4 +2,71 @@ package Aula04;
 
 public class RepositorioContasArray {
 
+	public static final int TAM_CACHE_CONTAS = 100;
+	private Conta contas[];
+	private int indice;
+	
+	
+	public RepositorioContasArray(){
+		indice = 0;
+		contas = new Conta [TAM_CACHE_CONTAS];		
+	}
+	
+	public void inserir(Conta c){
+		contas[indice] = c;
+		indice++;
+		
+	}
+	
+	public void atualizar (Conta c){
+		
+		
+	}
+	
+	public void remover (String numeroConta){
+		
+		if(existe(numeroConta)){
+			int i = procurarIndice(numeroConta);
+			contas[i] = contas[indice - 1];
+			contas[indice - 1] = null;
+			indice = indice - 1;
+			
+		}
+		
+	}
+	
+	private int procurarIndice(String numeroConta){
+		int i=0;
+		int ind =-1;
+		for(Conta c: contas){
+			if(c.getNumero().equals(numeroConta)){
+				ind=i;
+				break;				
+			}
+			i++;			
+		}
+		return ind;		
+	}
+	
+	public boolean existe (String numeroConta){
+		
+		boolean resp = false;
+		
+		int i = procurarIndice(numeroConta);
+		
+		if( i != -1)
+			resp = true;
+		
+		return resp;
+		
+	}
+	
+	public Conta procurar(String numeroConta){
+				
+	}
+	
+	
+	
+	
+	
 }
