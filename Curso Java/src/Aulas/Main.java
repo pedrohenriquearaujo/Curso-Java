a5e1872662e4f010583a45862383111d7e2cdc5a
Pedro Henrique <pedrohenriquearaujo@github.com>
2018-01-22 12:05:35 -0300

    Teste Das Classe

--- Curso Java/src/Aulas/Main.java
+++ Curso Java/src/Aulas/Main.java
@@ -10,20 +10,21 @@ public class Main {
 		Conta c2 = new Conta("789");
 		Conta c3 = new Poupanca("753");
 		Poupanca c4 = new Poupanca("234");
-		
-		
+				
 		r.inserir(c);
 		r.inserir(c1);
 		r.inserir(c2);
 		r.inserir(c3);
 		r.inserir(c4);
+	
 		c3.creditar(200);
 		((Poupanca)c3).renderJuros(2);
+		c3.debitar(20);
 		
 		c4.renderJuros(20);
 		
-		System.out.println((c3.getSaldo()));
-	
+		System.out.println(c3.getSaldo());
+		System.out.println(c3.getNumero());
 		
 		
 		
